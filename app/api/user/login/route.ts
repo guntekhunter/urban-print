2a5de137c2c1ec21d3edebbbff4793e471577e97
guntekhunter <guntek2000@gmail.com>
2024-06-14 21:29:21 +0800

    fead: login and register

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -1,28 +1,49 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  const reqBody = await req.json();
+export async function POST(req: NextRequest) {
   try {
-    const isUser = await prisma.user.findFirst({
+    const reqBody = await req.json();
+    const { email, password } = reqBody;
+    const user = await prisma.user.findUnique({
       where: {
-        email: reqBody.userEmail,
+        email,
       },
     });
-    let passwordInput = isUser?.password ?? "";
 
-    const comparePassword = await bcrypt.compare(
-      reqBody.password,
-      passwordInput
-    );
-    if (comparePassword) {
-      return NextResponse.json({ status: "ok", data: isUser });
+    if (!user) {
+      return NextResponse.json(
+        { error: "email tidak terdaftar" },
+        { status: 400 }
+      );
     }
-    return NextResponse.json({ status: "error", message: "wrong password" });
-  } catch (error) {
-    console.log(error);
+
+    const validatePassword = await bcrypt.compare(password, user.password);
+
+    if (!validatePassword) {
+      return NextResponse.json({ error: "password salah" }, { status: 400 });
+    }
+
+    //create token data
+    const tokenData = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY!, {
+      expiresIn: "1d",
+    });
+
+    // return response;
+    return NextResponse.json({ status: "Ok", user: user, token: token });
+
+    // console.log(serverRuntimeConfig.secret);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,100 @@
+'use client'
+import axios from "axios";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  const route = useRouter();
+
+  const handleLogin = async () => {
+    try{
+      setIsLoading(true)
+      const res = await axios.post("api/user/login", {
+        email: email,
+        password: password
+      })
+      console.log(res)
+      const type = res.data.user.type
+      if(type === "admin"){
+        route.push("/admin");
+      }else if(type === "operator"){
+        route.push("/operator");
+      }else if(type === "sales"){
+        route.push("/sales");
+      }else if(type === "manager"){
+        route.push("/manager");
+      }
+    }catch(error:any){
+      setError(error.response.data.error);
+      // console.log(error.response.data.error)
+    }finally{
+      setIsLoading(false)
+    }
+
+  }
   return (
-    <div>
-      ommaleka
+    <div className="h-screen flex items-center justify-center bg-gray-200">
+      <div className="p-[4rem] space-y-[1rem] w-[30rem] rounded-md shadow-md bg-white text-text">
+        <p className="flex justify-center font-bold">LOGIN</p>
+        <div className="w-full space-y-2">
+          <p className="text-[.9rem]">Email</p>
+          <input
+            className="w-full h-[2rem] px-[1rem] rounded-md border-[1.4px] border-gray-200 text-[.9rem]"
+            type="text"
+            autoComplete="off"
+            onChange={(e: any) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div className="space-y-2">
+          <p className="text-[.9rem]">Password</p>
+          <div>
+            <input
+              className="w-full h-[2rem] px-[1rem] rounded-md border-[1.4px] border-gray-200 text-[.9rem]"
+              type="password"
+              autoComplete="off"
+              onChange={(e: any) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Image src="" alt="" />
+          </div>
+        </div>
+        <div
+          className={`bg-red-200 h-[2rem] flex items-center pl-4 rounded-md border-[1.4px] border-red-300 ${
+            error ? "" : "hidden"
+          }`}
+        >
+          <p className={`text-[.8rem] text-red-500 ${error ? "" : "hidden"}`}>
+            {error}
+          </p>
+        </div>
+        <div className="flex justify-around pt-[1rem]">
+          <button
+            onClick={handleLogin}
+            className="px-[1rem] rounded-md bg-green-600 text-white w-full h-[2.5rem] hover:bg-green-600 flex justify-around items-center bg-primary"
+          >
+            {isLoading ? (
+              <Image
+                width={500}
+                height={500}
+                src="/spinner-of-dots.png"
+                alt=""
+                className="animate-spin w-[1rem] invert"
+              />
+            ) : (
+              <p>Login</p>
+            )}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
