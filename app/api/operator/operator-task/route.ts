b79983bfa0f229cb8c6d8fb1b26738f9e9a6017f
guntekhunter <guntek2000@gmail.com>
2024-06-25 21:45:06 +0800

    fead: api order length base on type of the order

---
+++ app/api/operator/operator-task/route.ts
@@ -0,0 +1,92 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const reqBody = await req.json();
+  try {
+    const countsPrinting = await Promise.all([
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "printing",
+          status: 1,
+        },
+      }),
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "printing",
+          status: 2,
+        },
+      }),
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "printing",
+          status: 3,
+        },
+      }),
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "printing",
+          status: 4,
+        },
+      }),
+    ]);
+
+    // Create a simplified response object
+    const responsePrinting = {
+      notStarted: countsPrinting[0],
+      waiting: countsPrinting[1],
+      onProgress: countsPrinting[2],
+      finish: countsPrinting[3],
+    };
+    const countsFinishing = await Promise.all([
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "finishing",
+          status: 1,
+        },
+      }),
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "finishing",
+          status: 2,
+        },
+      }),
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "finishing",
+          status: 3,
+        },
+      }),
+      prisma.order.count({
+        where: {
+          id_operator: reqBody.id,
+          product_type: "finishing",
+          status: 4,
+        },
+      }),
+    ]);
+
+    // Create a simplified response object
+    const responseFinishing = {
+      notStarted: countsFinishing[0],
+      waiting: countsFinishing[1],
+      onProgress: countsFinishing[2],
+      finish: countsFinishing[3],
+    };
+    return NextResponse.json({
+      printing: responsePrinting,
+      finishing: responseFinishing,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
